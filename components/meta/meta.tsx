4737030e5afef1b06a841a31618c9367eced858a
Luis Manuel González <mannyglzg@gmail.com>
2021-05-22 00:22:58 -0700

    Implement design in showcase

--- components/meta/meta.tsx
+++ components/meta/meta.tsx
@@ -6,7 +6,7 @@ const Meta = () => {
       <link rel="icon" href="/images/favicon-32x32.png" />
       <link rel="preconnect" href="https://fonts.gstatic.com" />
       <link
-        href="https://fonts.googleapis.com/css2?family=Public+Sans:wght@300;400;500&display=swap"
+        href="https://fonts.googleapis.com/css2?family=Public+Sans:wght@300;400;700&display=swap"
         rel="stylesheet"
       />
     </Head>
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,17 +2,61 @@ import Head from 'next/head';
 import Image from 'next/image';
 import Layout from '../components/layout';
 import Header from '../components/header';
+import Container from '../components/container';
+import Button from '../components/button';
 import styles from '../styles/Home.module.scss';
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div>
       <Layout>
         <Head>
           <title>Easybank</title>
         </Head>
 
         <Header />
+
+        <section className={styles.showcase}>
+          <Container>
+            <div className={styles.showcaseContainer}>
+              <div className={styles.showcaseIntro}>
+                <h2>Next generation digital banking</h2>
+                <p>
+                  Take your financial life online. Your Easybank account
+                  <br /> will be one-stop-shop for spending, saving,
+                  <br /> budgeting, investing, and much more.
+                </p>
+                <Button>Request Invite</Button>
+              </div>
+              <div>
+                <img
+                  className={styles.bgIntroDesktop}
+                  src="/images/bg-intro-desktop.svg"
+                  alt="Intro"
+                  width="1200"
+                />
+                <img
+                  className={styles.bgIntroMockups}
+                  src="/images/image-mockups.png"
+                  alt="App mockups"
+                  width="720"
+                />
+              </div>
+            </div>
+          </Container>
+        </section>
+        <section className={styles.benefits}>
+          <Container>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Autem,
+            veritatis.
+          </Container>
+        </section>
+        <section className={styles.latestArticles}>
+          <Container>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus,
+            ipsa!
+          </Container>
+        </section>
       </Layout>
     </div>
   );
