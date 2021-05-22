5b03de2935f4bd753318ae5000d8e56a23cc57a3
Luis Manuel González <mannyglzg@gmail.com>
2021-05-22 02:14:34 -0700

    Add article cards

--- pages/index.tsx
+++ pages/index.tsx
@@ -47,6 +47,7 @@ export default function Home() {
             </div>
           </Container>
         </section>
+
         <section className={styles.benefits}>
           <Container>
             <div>
@@ -134,10 +135,88 @@ export default function Home() {
             </div>
           </Container>
         </section>
+
         <section className={styles.latestArticles}>
           <Container>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus,
-            ipsa!
+            <h2>Latest Articles</h2>
+            <div className={styles.articlesList}>
+              <div className={styles.articleItem}>
+                <img
+                  className={styles.articleImage}
+                  src="/images/image-currency.jpg"
+                  alt="Currency"
+                />
+                <div className={styles.articleBody}>
+                  <span className={styles.articleAuthor}>
+                    By Claire Robinson
+                  </span>
+                  <h4 className={styles.articleTitle}>
+                    Receive money in any currency with no fees
+                  </h4>
+                  <p className={styles.articleContent}>
+                    The world is getting smaller and we're becoming more mobile.
+                    So why should you be forced to only receive money in a
+                    single ...
+                  </p>
+                </div>
+              </div>
+              <div className={styles.articleItem}>
+                <img
+                  className={styles.articleImage}
+                  src="/images/image-restaurant.jpg"
+                  alt="Restaurant"
+                />
+                <div className={styles.articleBody}>
+                  <span className={styles.articleAuthor}>By Wilson Hutton</span>
+                  <h4 className={styles.articleTitle}>
+                    Treat yourself without worrying about money
+                  </h4>
+                  <p className={styles.articleContent}>
+                    Our simple budgeting feature allows you to separate out your
+                    spending and set realistic limits each month. That means you
+                    ...
+                  </p>
+                </div>
+              </div>
+              <div className={styles.articleItem}>
+                <img
+                  className={styles.articleImage}
+                  src="/images/image-plane.jpg"
+                  alt="Plane"
+                />
+                <div className={styles.articleBody}>
+                  <span className={styles.articleAuthor}>By Wilson Hutton</span>
+                  <h4 className={styles.articleTitle}>
+                    Take your Easybank card wherever you go
+                  </h4>
+                  <p className={styles.articleContent}>
+                    We want you to enjoy your travels. This is why we don't
+                    charge any fees on purchases while you're abroad. We'll even
+                    show you ...
+                  </p>
+                </div>
+              </div>
+              <div className={styles.articleItem}>
+                <img
+                  className={styles.articleImage}
+                  src="/images/image-confetti.jpg"
+                  alt="Confetti"
+                />
+                <div className={styles.articleBody}>
+                  <span className={styles.articleAuthor}>
+                    By Claire Robinson
+                  </span>
+                  <h4 className={styles.articleTitle}>
+                    Our invite-only Beta accounts are now live!
+                  </h4>
+                  <p className={styles.articleContent}>
+                    After a lot of hard work by the whole team, we're excited to
+                    launch our closed beta. It's easy to request an invite
+                    through the site ...
+                  </p>
+                </div>
+              </div>
+            </div>
           </Container>
         </section>
       </Layout>
