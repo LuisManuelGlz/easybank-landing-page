1f77da20ef303ab36e0983fde428fc5689be9efe
Luis Manuel González <mannyglzg@gmail.com>
2021-05-22 20:47:02 -0700

    Resize background

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,10 @@
-import Head from 'next/head';
-import Image from 'next/image';
-import Layout from '../components/layout';
-import Header from '../components/header';
-import Container from '../components/container';
-import Button from '../components/button';
-import styles from '../styles/Home.module.scss';
+import Head from "next/head";
+import Image from "next/image";
+import Layout from "../components/layout";
+import Header from "../components/header";
+import Container from "../components/container";
+import Button from "../components/button";
+import styles from "../styles/Home.module.scss";
 
 export default function Home() {
   return (
@@ -19,33 +19,32 @@ export default function Home() {
         {/* Showcase */}
         <section className={styles.showcase}>
           <Container>
-            <div className={styles.showcaseContainer}>
-              <div className={styles.showcaseIntro}>
-                <h2>Next generation digital banking</h2>
-                <p>
-                  Take your financial life online. Your Easybank account
-                  <br />
-                  will be one-stop-shop for spending, saving,
-                  <br />
-                  budgeting, investing, and much more.
-                </p>
-                <Button>Request Invite</Button>
-              </div>
-              <div>
-                <img
-                  className={styles.bgIntroDesktop}
-                  src="/images/bg-intro-desktop.svg"
-                  alt="Intro"
-                  width="1200"
-                />
-                <img
-                  className={styles.bgIntroMockups}
-                  src="/images/image-mockups.png"
-                  alt="App mockups"
-                  width="720"
-                />
-              </div>
-            </div>
+            <h2>
+              Next generation
+              <br />
+              digital banking
+            </h2>
+
+            <p>
+              Take your financial life online. Your Easybank account
+              <br />
+              will be one-stop-shop for spending, saving,
+              <br />
+              budgeting, investing, and much more.
+            </p>
+
+            <Button>Request Invite</Button>
+
+            <img
+              className={styles.bgIntroDesktop}
+              src="/images/bg-intro-desktop.svg"
+              alt="Intro"
+            />
+            <img
+              className={styles.bgIntroMockups}
+              src="/images/image-mockups.png"
+              alt="App mockups"
+            />
           </Container>
         </section>
 
