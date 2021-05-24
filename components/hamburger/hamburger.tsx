bd27f638e7ab2f97bf1070e7f31ec573e290ffe5
Luis Manuel González <mannyglzg@gmail.com>
2021-05-24 10:26:11 -0700

    Implement hamburger button functionality
    Dependencies installed:
    * react-modal

--- components/hamburger/hamburger.tsx
+++ components/hamburger/hamburger.tsx
@@ -1,14 +1,19 @@
-import { MouseEvent } from "react";
-import styles from "./hamburger.module.scss";
+import { MouseEvent } from 'react';
+import styles from './hamburger.module.scss';
 
 type Props = {
   onPress: (event: MouseEvent<HTMLButtonElement>) => void;
+  isOpen: boolean;
 };
 
-const Hamburger = ({ onPress }: Props) => {
+const Hamburger = ({ onPress, isOpen }: Props) => {
   return (
     <button className={styles.container} onClick={onPress}>
-      <img src="/images/icon-hamburger.svg" alt="Hamburger" />
+      {isOpen ? (
+        <img src="/images/icon-close.svg" alt="Hamburger" />
+      ) : (
+        <img src="/images/icon-hamburger.svg" alt="Hamburger" />
+      )}
     </button>
   );
 };
--- components/header/header.tsx
+++ components/header/header.tsx
@@ -1,9 +1,27 @@
+import { useEffect, useState } from 'react';
+import Modal from 'react-modal';
 import Container from '../container';
 import Button from '../button';
 import Hamburger from '../hamburger';
 import styles from './header.module.scss';
 
 const Header = () => {
+  const [modalIsOpen, setIsOpen] = useState(false);
+
+  function toggleModal() {
+    setIsOpen(!modalIsOpen);
+  }
+
+  useEffect(() => {
+    const handleResize = () => {
+      const { innerWidth } = window;
+      innerWidth > 992 && setIsOpen(false);
+    };
+
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
   return (
     <nav className={styles.navigation}>
       <Container>
@@ -34,9 +52,36 @@ const Header = () => {
           </div>
 
           <div className={styles.navHamburger}>
-            <Hamburger onPress={console.log} />
+            <Hamburger onPress={toggleModal} isOpen={modalIsOpen} />
           </div>
         </div>
+        <Modal
+          className={styles.navModal}
+          overlayClassName={styles.navModalOverlay}
+          isOpen={modalIsOpen}
+          onRequestClose={toggleModal}
+          ariaHideApp={false}
+          shouldCloseOnOverlayClick={true}
+          contentLabel="Nav Modal"
+        >
+          <div className={styles.modalContainer}>
+            <a href="#" onClick={toggleModal}>
+              Home
+            </a>
+            <a href="#" onClick={toggleModal}>
+              About
+            </a>
+            <a href="#" onClick={toggleModal}>
+              Contact
+            </a>
+            <a href="#" onClick={toggleModal}>
+              Blog
+            </a>
+            <a href="#" onClick={toggleModal}>
+              Careers
+            </a>
+          </div>
+        </Modal>
       </Container>
     </nav>
   );
